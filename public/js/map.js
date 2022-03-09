6e3dc7934305233cf3266a16c72c9eeb956fc75a
gezlev <gezlev@ukr.net>
2022-03-09 23:26:15 +0200

    add datepicker

---
+++ public/js/map.js
@@ -0,0 +1,15 @@
+﻿const warMap = function(target) {
+    this.map = L.map(target).setView({lon: 24, lat: 51}, 8);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(this.map);
+    L.control.scale({imperial: true, metric: true}).addTo(this.map);
+
+    this.map.on("click", function(pos) {
+        console.log(pos);
+        getAddressName(pos.latlng)
+    });
+}
+
+
